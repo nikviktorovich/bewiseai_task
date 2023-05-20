88708ff818a8d61c37d0630ddc5b3cafd535d90c
lnfecteDru <nikitasmn98@gmail.com>
2023-05-20 19:12:57 +0300

    Fixed error message on creating new instance

--- src/quiz_task/modules/quiz/domain/models.py
+++ src/quiz_task/modules/quiz/domain/models.py
@@ -1,10 +1,11 @@
 import dataclasses
 import datetime
+from typing import Any
 
 
 @dataclasses.dataclass
 class Quiz:
-    id: int
+    id: Any
     question: str
     answer: str
     created_at: datetime.datetime
