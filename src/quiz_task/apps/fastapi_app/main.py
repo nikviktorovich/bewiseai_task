b261f7451706aa17ad4d78eb7e7c002d3041bb9d
lnfecteDru <nikitasmn98@gmail.com>
2023-05-21 17:57:23 +0300

    Added handling repository not found error

--- src/quiz_task/apps/fastapi_app/main.py
+++ src/quiz_task/apps/fastapi_app/main.py
@@ -1,6 +1,9 @@
 import fastapi
+import fastapi.responses
+from fastapi import status
 
 import quiz_task.apps.fastapi_app.routers.quiz.endpoints
+import quiz_task.common.errors
 import quiz_task.database.mappers
 
 
@@ -9,6 +12,15 @@ def on_startup():
 
 
 app = fastapi.FastAPI(on_startup=[on_startup])
+
+
+@app.exception_handler(quiz_task.common.errors.EntityNotFoundError)
+def handle_entity_not_found_error(request, exception):
+    return fastapi.responses.JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND,
+        content=str(exception),
+    )
+
 
 # Routers
 app.include_router(quiz_task.apps.fastapi_app.routers.quiz.endpoints.router)
